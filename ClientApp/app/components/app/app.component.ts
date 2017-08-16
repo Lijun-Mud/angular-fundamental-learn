16b12b827718230aec49b4efa442b74d67799e93
LIJUN <mudfans999@live.com>
2017-08-16 15:10:11 +0800

    use import to get css file in css file

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css', '../../../../node_modules/ngx-toastr/toastr.css'],
+    styleUrls: ['./app.component.css'],
     encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
